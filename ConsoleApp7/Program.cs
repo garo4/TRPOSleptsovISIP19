ca688879b8b5e4139d5fb648b3347ff18a33d747
Nikita Sleptsov <nik.sleptsov56@mail.ru>
2020-09-14 12:50:21 +0900

    first commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            double S, c, P;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            c = Math.Sqrt(x * x + y * y);
+            P = x + y + c;
+            S = x * y * 0.5;
+            Console.WriteLine("гипотенуза = " + c);
+            Console.WriteLine("площадь = " + S);
+            Console.WriteLine("периметр = " + P);
+            Console.ReadKey();
+        }
+    }
+}
